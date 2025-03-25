f44fa67ce33d73c9b8613faca2f08eab45957a09
AB <a.bierler@xdev-software.de>
2025-03-25 11:48:19 +0100

    Make remember me work

--- demo/persistence/src/main/resources/flyway/structure/B1.1_Init.sql
+++ demo/persistence/src/main/resources/flyway/structure/B1.1_Init.sql
@@ -8,8 +8,8 @@ ALTER TABLE product ADD CONSTRAINT uc_product_name UNIQUE (name);
 
 CREATE TABLE `userdetail` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email_address` varchar(255) NOT NULL,
-  `fullname` varchar(255) NOT NULL DEFAULT '',
+  `email_address` VARCHAR(255) NOT NULL,
+  `fullname` VARCHAR(255) NOT NULL DEFAULT '',
   `created_at` datetime NOT NULL,
   `disabled_at` datetime DEFAULT NULL,
   `last_login_at` datetime DEFAULT NULL,
@@ -19,10 +19,11 @@ CREATE TABLE `userdetail` (
 
 CREATE TABLE `auth_remember_me_secret` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(255) NOT NULL,
+  `identifier` VARCHAR(255) NOT NULL,
   `secret` blob NOT NULL,
   `created_at` datetime NOT NULL,
   `user_id` bigint(20) NOT NULL,
+  `crypto_algorithm` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uc_auth_remember_me_secret_identifier` (`identifier`),
   KEY `FK_AUTH_REMEMBER_ME_SECRET_ON_USER` (`user_id`),
