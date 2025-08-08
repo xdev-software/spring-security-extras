d0c1fff6199624507ac06e4e29bfd498c2593e65
AB <a.bierler@xdev-software.de>
2025-08-08 09:13:31 +0200

    Do not build/resolve integration-test project when running tests

--- demo/integration-tests/tci-webapp/src/main/java/software/xdev/sse/demo/tci/webapp/containers/WebAppContainerBuilder.java
+++ demo/integration-tests/tci-webapp/src/main/java/software/xdev/sse/demo/tci/webapp/containers/WebAppContainerBuilder.java
@@ -1,14 +1,18 @@
 package software.xdev.sse.demo.tci.webapp.containers;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import software.xdev.testcontainers.imagebuilder.AdvancedImageFromDockerFile;
 import software.xdev.testcontainers.imagebuilder.compat.DockerfileCOPYParentsEmulator;
+import software.xdev.testcontainers.imagebuilder.transfer.fcm.FileLinesContentModifier;
 
 
 @SuppressWarnings("PMD.MoreThanOneLogger")
@@ -52,7 +56,7 @@ public final class WebAppContainerBuilder
 					"Dockerfile",
 					// Ignore not required test-modules that may have changed
 					// sources only - otherwise the parent pom doesn't find the resources
-					"demo/integration-tests/*/src/**",
+					"demo/integration-tests/**",
 					"**/src/test/**",
 					// Ignore resources that are just used for development
 					"demo/webapp-rest/src/main/resources-dev/**",
@@ -61,7 +65,40 @@ public final class WebAppContainerBuilder
 					"target")
 				.withDockerFilePath(Paths.get("../../../demo/integration-tests/" + module + "/Dockerfile"))
 				.withBaseDir(Paths.get("../../../"))
-				.withDockerFileLinesModifier(new DockerfileCOPYParentsEmulator());
+				.withDockerFileLinesModifier(new DockerfileCOPYParentsEmulator())
+				.withTransferArchiveTARCompressorCustomizer(c -> c
+					// Rewrite parent pom to exclude integration tests
+					// This way changes in test pom's cause no redownload of dependencies
+					.withContentModifier(new FileLinesContentModifier()
+					{
+						@Override
+						public boolean shouldApply(
+							final Path sourcePath,
+							final String targetPath,
+							final TarArchiveEntry tarArchiveEntry)
+						{
+							return "demo/pom.xml".equals(targetPath);
+						}
+						
+						@Override
+						public List<String> modify(
+							final List<String> lines,
+							final Path sourcePath,
+							final String targetPath,
+							final TarArchiveEntry tarArchiveEntry)
+						{
+							return lines.stream()
+								// Remove integration tests module
+								.filter(s -> !s.contains("<module>integration-tests"))
+								.toList();
+						}
+						
+						@Override
+						public boolean isIdentical(final List<String> original, final List<String> created)
+						{
+							return original.size() == created.size();
+						}
+					}));
 		
 		try
 		{
