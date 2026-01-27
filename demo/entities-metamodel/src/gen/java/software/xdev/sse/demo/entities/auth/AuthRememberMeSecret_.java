4c77088817cc5eb62479a25bf73a292e36358a23
AB <a.bierler@xdev-software.de>
2026-01-27 10:28:59 +0100

    Update hibernate processor

--- demo/entities-metamodel/src/gen/java/software/xdev/sse/demo/entities/auth/AuthRememberMeSecret_.java
+++ demo/entities-metamodel/src/gen/java/software/xdev/sse/demo/entities/auth/AuthRememberMeSecret_.java
@@ -3,13 +3,16 @@ package software.xdev.sse.demo.entities.auth;
 import jakarta.persistence.metamodel.EntityType;
 import jakarta.persistence.metamodel.StaticMetamodel;
 
+/**
+ * Static metamodel for {@link software.xdev.sse.demo.entities.auth.AuthRememberMeSecret}
+ **/
 @StaticMetamodel(AuthRememberMeSecret.class)
-public abstract class AuthRememberMeSecret_ extends software.xdev.sse.demo.entities.auth.Secret_ {
+public abstract class AuthRememberMeSecret_ extends Secret_ {
 
 
 	
 	/**
-	 * @see software.xdev.sse.demo.entities.auth.AuthRememberMeSecret
+	 * Static metamodel type for {@link software.xdev.sse.demo.entities.auth.AuthRememberMeSecret}
 	 **/
 	public static volatile EntityType<AuthRememberMeSecret> class_;
 
