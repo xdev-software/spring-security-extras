f1bbaa583d17904c252dc3a14af99f7c0e9f48aa
AB <a.bierler@xdev-software.de>
2026-07-17 08:55:17 +0200

    Migrate DB to TCI v4

--- demo/integration-tests/tci-db/src/main/java/software/xdev/sse/demo/tci/db/factory/DBTCIFactory.java
+++ demo/integration-tests/tci-db/src/main/java/software/xdev/sse/demo/tci/db/factory/DBTCIFactory.java
@@ -1,8 +1,13 @@
 package software.xdev.sse.demo.tci.db.factory;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
 import software.xdev.sse.demo.tci.db.DBTCI;
 import software.xdev.sse.demo.tci.db.containers.DBContainer;
 import software.xdev.sse.demo.tci.db.containers.DBContainerBuilder;
+import software.xdev.tci.concurrent.Suppliers;
+import software.xdev.tci.concurrent.TCIExecutorServiceHolder;
 import software.xdev.tci.db.factory.BaseDBTCIFactory;
 import software.xdev.tci.factory.prestart.snapshoting.CommitedImageSnapshotManager;
 import software.xdev.tci.misc.ContainerMemory;
@@ -10,6 +15,9 @@ import software.xdev.tci.misc.ContainerMemory;
 
 public class DBTCIFactory extends BaseDBTCIFactory<DBContainer, DBTCI>
 {
+	protected static final Supplier<String> IMAGE_NAME_SUPPLIER =
+		Suppliers.memoize(DBContainerBuilder::getImageName);
+	
 	public DBTCIFactory()
 	{
 		this(true);
@@ -20,7 +28,7 @@ public class DBTCIFactory extends BaseDBTCIFactory<DBContainer, DBTCI>
 	{
 		super(
 			(c, n) -> new DBTCI(c, n, migrateAndInitializeEMC),
-			() -> new DBContainer(DBContainerBuilder.getBuiltImageName())
+			() -> new DBContainer(IMAGE_NAME_SUPPLIER.get())
 				.withDatabaseName(DBTCI.DB_DATABASE)
 				.withCreateContainerCmdModifier(cmd -> cmd.getHostConfig().withMemory(ContainerMemory.M512M)),
 			"db-mariadb",
@@ -32,7 +40,7 @@ public class DBTCIFactory extends BaseDBTCIFactory<DBContainer, DBTCI>
 	@Override
 	protected void warmUpInternal()
 	{
-		DBContainerBuilder.getBuiltImageName();
+		CompletableFuture.runAsync(IMAGE_NAME_SUPPLIER::get, TCIExecutorServiceHolder.instance());
 		super.warmUpInternal();
 	}
 }
